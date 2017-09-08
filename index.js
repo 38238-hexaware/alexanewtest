f5165cec658b1b43081dd10029ab9154c6b5cb3c
Srinivasan Velu <38238@corp.hexaware.com>
2017-09-08 12:27:07 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+//const request = require('request-promise')  ;
+var fetch = require('node-fetch');
+var express=require('express');
+var app=express();
+// const options = {  
+//   method: 'GET',
+//   uri: 'http://widgets.fabulously40.com/horoscope.json?sign=capricorn',
+// }
+// request(options)  
+//   .then(function (response) {
+//     console.log(JSON.stringify(response));
+//   })
+//   .catch(function (err) {
+//   console.log(JSON.stringify(err));
+// })
+// const unhandledRejection = require("unhandled-rejection");
+// let rejectionEmitter = unhandledRejection({
+//     timeout: 20
+// });
+// rejectionEmitter.on("unhandledRejection", (error, promise) => {
+//     console.log(JSON.stringify(error));
+// });
+
+// rejectionEmitter.on("rejectionHandled", (error, promise) => {
+//         console.log(JSON.stringify(error));
+// })
+// fetch('http://widgets.fabulously40.com/horoscope.json?sign=capricorn')
+//     .then(function(res) {
+//         console.log(res);
+//         return res.text();
+//     }).then(function(body) {
+//         console.log(body);
+//     });
+
+app.listen("3000",function(){
+    console.log('listeneing at the port');
+})
